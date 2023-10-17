ef4fe641cf099722e54e75aa2bcc590ebcd2c08a
Matt Hansen <matt.hansen@luminartech.com>
2023-10-17 14:25:02 -0400

    rename output

--- make_maps.sh
+++ make_maps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 ./shapefile_to_svg.py -o madison.svg -b input_3d/MBSP_3Dpas.zip
-python3 ./shapefile_to_svg.py -o pot.svg --bounding_box input_3d/MBSP_3Dpas.zip input_3d/PotSpring_3Dpas.zip
+python3 ./shapefile_to_svg.py -o pot_spring.svg --bounding_box input_3d/MBSP_3Dpas.zip input_3d/PotSpring_3Dpas.zip
