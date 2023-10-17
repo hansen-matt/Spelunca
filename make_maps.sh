529b871204172159b35916b43188b8c4d766f48d
Matt Hansen <matt.hansen@luminartech.com>
2023-10-17 14:26:11 -0400

    More informative output

--- make_maps.sh
+++ make_maps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo "Making Madison map"
 python3 ./shapefile_to_svg.py -o madison.svg -b input_3d/MBSP_3Dpas.zip
+echo ""
+
+echo "Making Pot Spring map"
 python3 ./shapefile_to_svg.py -o pot_spring.svg --bounding_box input_3d/MBSP_3Dpas.zip input_3d/PotSpring_3Dpas.zip
