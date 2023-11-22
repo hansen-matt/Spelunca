091c09595bc2f315189c5b63f97340eb67a2e995
DEKHTIARJonathan <jonathan@dekhtiar.com>
2023-11-22 11:40:48 -0500

    svg_py submodule removal & Dev Container Addition

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Tools and data for the Madison map.
 
 ## Python requirements
 
-python3 -m pip install fiona pyshp svgwrite pyproj matplotlib
+python3 -m pip install -r requirements.txt
--- make_maps.sh
+++ make_maps.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 echo "Making Madison map"
-python3 ./shapefile_to_svg.py -o madison.svg input_3d/MBSP_3Dpas.zip
+python3 ./shapefile_to_svg.py -o outputs/madison.svg input_3d/MBSP_3Dpas.zip
 echo ""
 
 echo "Making Pot Spring map"
-python3 ./shapefile_to_svg.py -o pot_spring.svg --bounding_box input_3d/MBSP_3Dpas.zip input_3d/PotSpring_3Dpas.zip
+python3 ./shapefile_to_svg.py -o outputs/pot_spring.svg --bounding_box input_3d/MBSP_3Dpas.zip input_3d/PotSpring_3Dpas.zip
 echo ""
 
 echo "Making Cavern inset"
-python3 ./shapefile_to_svg.py -o madison_inset.svg --inset_x1 47 --inset_x2 65 --inset_y1 23 --inset_y2 40 --max_depth -100 input_3d/MBSP_3Dpas.zip
+python3 ./shapefile_to_svg.py -o outputs/madison_inset.svg --inset_x1 47 --inset_x2 65 --inset_y1 23 --inset_y2 40 --max_depth -100 input_3d/MBSP_3Dpas.zip
 echo ""
 
 echo "Making depth scale"
-python3 ./shapefile_to_svg.py -o depth_scale.svg --depth_scale input_3d/MBSP_3Dpas.zip
+python3 ./shapefile_to_svg.py -o outputs/depth_scale.svg --depth_scale input_3d/MBSP_3Dpas.zip
 echo ""
