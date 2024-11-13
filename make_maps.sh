4852c9e8616f3235fc2252369493afc3d8000058
Matt Hansen <confuted@gmail.com>
2024-11-13 16:28:37 -0500

    start generating images for a banner

--- make_maps.sh
+++ make_maps.sh
@@ -33,3 +33,12 @@ echo ""
 echo "Making Manatee depth scale"
 python3 ./spelunca.py -o $OUTPUT_PATH/manatee/depth_scale.svg --depth_scale $INPUT_PATH/Manatee_3Dpas.zip
 echo ""
+
+echo "Making Withlacoochee banner"
+mkdir -p $OUTPUT_PATH/banner
+python3 ./spelunca.py --width 33.7 --height 81.34 -o $OUTPUT_PATH/banner/madison_banner.svg --bounding_box $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/MBSP_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 -o $OUTPUT_PATH/banner/M2_banner.svg $INPUT_PATH/'M2 Blue Cave_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 -o $OUTPUT_PATH/banner/pot_banner.svg --bounding_box $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/PotSpring_3Dpas.zip
+echo ""
+
+
