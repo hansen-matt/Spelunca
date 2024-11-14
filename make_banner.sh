714e2e57ac6d0c5870a56b31d1c7c3c781e391b5
Matt Hansen <confuted@gmail.com>
2024-11-14 16:37:19 -0500

    TODO comment on depth scaling

--- make_banner.sh
+++ make_banner.sh
@@ -6,6 +6,7 @@ echo "BOUNDING BOX"
 BOUNDING_BOX=$(python3 ./find_bounding_box.py $INPUT_PATH/Agate-Hardee_3Dpas.zip $INPUT_PATH/PotSpring_3Dpas.zip $INPUT_PATH/MBSP_3Dpas.zip $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/'M2 Blue Cave_3Dpas.zip')
 
 echo "Making Withlacoochee banner"
+# TODO: fix depth scaling. It needs to be the same for every cave.
 mkdir -p $OUTPUT_PATH/banner
 python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/hardee_banner.svg         $INPUT_PATH/Agate-Hardee_3Dpas.zip
 python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/madison_banner.svg        $INPUT_PATH/MBSP_3Dpas.zip
