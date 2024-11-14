309feae9169231ee59d967c6abd2d362f009a810
Matt Hansen <confuted@gmail.com>
2024-11-14 16:43:51 -0500

    set the scale factor for the banner

--- make_banner.sh
+++ make_banner.sh
@@ -2,16 +2,18 @@
 INPUT_PATH="../cave_maps/input_3d"
 OUTPUT_PATH="../cave_maps/output"
 
+SCALE="--scale_factor 40"
+
 echo "BOUNDING BOX"
-BOUNDING_BOX=$(python3 ./find_bounding_box.py $INPUT_PATH/Agate-Hardee_3Dpas.zip $INPUT_PATH/PotSpring_3Dpas.zip $INPUT_PATH/MBSP_3Dpas.zip $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/'M2 Blue Cave_3Dpas.zip')
+BOUNDING_BOX=$(python3 ./find_bounding_box.py $SCALE $INPUT_PATH/Agate-Hardee_3Dpas.zip $INPUT_PATH/PotSpring_3Dpas.zip $INPUT_PATH/MBSP_3Dpas.zip $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/'M2 Blue Cave_3Dpas.zip')
 
 echo "Making Withlacoochee banner"
 # TODO: fix depth scaling. It needs to be the same for every cave.
 mkdir -p $OUTPUT_PATH/banner
-python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/hardee_banner.svg         $INPUT_PATH/Agate-Hardee_3Dpas.zip
-python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/madison_banner.svg        $INPUT_PATH/MBSP_3Dpas.zip
-python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/M2_banner.svg             $INPUT_PATH/'M2 Blue Cave_3Dpas.zip'
-python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/pot_banner.svg            $INPUT_PATH/PotSpring_3Dpas.zip
-python3 ./spelunca.py --width 33.7 --height 81.34 -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_coochee_banner.svg $INPUT_PATH/'little coochie_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/hardee_banner.svg         $INPUT_PATH/Agate-Hardee_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/madison_banner.svg        $INPUT_PATH/MBSP_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/M2_banner.svg             $INPUT_PATH/'M2 Blue Cave_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/pot_banner.svg            $INPUT_PATH/PotSpring_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE -r --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_coochee_banner.svg $INPUT_PATH/'little coochie_3Dpas.zip'
 echo ""
 
