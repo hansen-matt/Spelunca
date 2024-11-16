76d13eaac5df7a9efc5b249d0ce7d1c52533c3b3
Matt Hansen <confuted@gmail.com>
2024-11-15 19:46:37 -0500

    add little otter

--- make_banner.sh
+++ make_banner.sh
@@ -5,7 +5,7 @@ OUTPUT_PATH="../cave_maps/output"
 SCALE="--scale_factor 40"
 
 echo "BOUNDING BOX"
-BOUNDING_BOX=$(python3 ./find_bounding_box.py $SCALE $INPUT_PATH/Agate-Hardee_3Dpas.zip $INPUT_PATH/PotSpring_3Dpas.zip $INPUT_PATH/MBSP_3Dpas.zip $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/'M2 Blue Cave_3Dpas.zip')
+BOUNDING_BOX=$(python3 ./find_bounding_box.py $SCALE $INPUT_PATH/Agate-Hardee_3Dpas.zip $INPUT_PATH/PotSpring_3Dpas.zip $INPUT_PATH/MBSP_3Dpas.zip $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/'M2 Blue Cave_3Dpas.zip' $INPUT_PATH/'Little Otter Sink_3Dpas.zip')
 
 echo "Making Withlacoochee banner"
 mkdir -p $OUTPUT_PATH/banner
@@ -14,5 +14,6 @@ python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOU
 python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/M2_banner.svg             $INPUT_PATH/'M2 Blue Cave_3Dpas.zip'
 python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/pot_banner.svg            $INPUT_PATH/PotSpring_3Dpas.zip
 python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_coochee_banner.svg $INPUT_PATH/'little coochie_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_otter_banner.svg $INPUT_PATH/'Little Otter Sink_3Dpas.zip'
 echo ""
 
