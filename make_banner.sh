71e0046ad1d05a63e32412cf8723a3f808030cc4
Matt Hansen <confuted@gmail.com>
2024-12-02 12:10:27 -0500

    Add david's disappointment to the banner

--- make_banner.sh
+++ make_banner.sh
@@ -9,11 +9,12 @@ BOUNDING_BOX=$(python3 ./find_bounding_box.py $SCALE $INPUT_PATH/Agate-Hardee_3D
 
 echo "Making Withlacoochee banner"
 mkdir -p $OUTPUT_PATH/banner
-python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/hardee_banner.svg         $INPUT_PATH/Agate-Hardee_3Dpas.zip
-python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/madison_banner.svg        $INPUT_PATH/MBSP_3Dpas.zip
-python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/M2_banner.svg             $INPUT_PATH/'M2 Blue Cave_3Dpas.zip'
-python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/pot_banner.svg            $INPUT_PATH/PotSpring_3Dpas.zip
-python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_coochee_banner.svg $INPUT_PATH/'little coochie_3Dpas.zip'
-python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_otter_banner.svg $INPUT_PATH/'Little Otter Sink_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/hardee_banner.svg                $INPUT_PATH/Agate-Hardee_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/madison_banner.svg               $INPUT_PATH/MBSP_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/M2_banner.svg                    $INPUT_PATH/'M2 Blue Cave_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/pot_banner.svg                   $INPUT_PATH/PotSpring_3Dpas.zip
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_coochee_banner.svg        $INPUT_PATH/'little coochie_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/little_otter_banner.svg          $INPUT_PATH/'Little Otter Sink_3Dpas.zip'
+python3 ./spelunca.py --width 33.7 --height 81.34 $SCALE --set_bounding_box $BOUNDING_BOX -o $OUTPUT_PATH/banner/davids_disappointment_banner.svg $INPUT_PATH/DavidsDisappointment_3Dpas.zip
 echo ""
 
